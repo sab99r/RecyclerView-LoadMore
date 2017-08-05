65875d9cd5986a8010003d4e6ef9a86b7f032a81
Sabeer Mohammed <imsab99r@gmail.com>
2017-08-05 21:01:37 +0530

    Added movies.php
    This is just a demo php file created for this example.

---
+++ movies.php
@@ -0,0 +1,128 @@
+<?php
+$movies = array(
+array('title'=>'The Shawshank Redemption (1994)','rating'=>'9.2'),		
+array('title'=>'The Godfather (1972)','rating'=>'9.2'),		
+array('title'=>'The Godfather: Part II (1974)','rating'=>'9.0'),		
+array('title'=>'The Dark Knight (2008)','rating'=>'8.9'),		
+array('title'=>'Schindlers List (1993)','rating'=>'8.9'),		
+array('title'=>'12 Angry Men (1957)','rating'=>'8.9'),		
+array('title'=>'Pulp Fiction (1994)','rating'=>'8.9'),		
+array('title'=>'The Lord of the Rings: The Return of the King (2003)','rating'=>'8.9'),		
+array('title'=>'The Good, the Bad and the Ugly (1966)','rating'=>'8.9'),		
+array('title'=>'Fight Club (1999)','rating'=>'8.8'),		
+array('title'=>'The Lord of the Rings: The Fellowship of the Ring (2001)','rating'=>'8.8'),		
+array('title'=>'Star Wars Episode V  The Empire Strikes Back (1980)','rating'=>'8.7'),		
+array('title'=>'Forrest Gump (1994)','rating'=>'8.7'),		
+array('title'=>'Inception (2010)','rating'=>'8.7'),		
+array('title'=>'The Lord of the Rings: The Two Towers (2002)','rating'=>'8.7'),		
+array('title'=>'One Flew Over the Cuckoos Nest (1975)','rating'=>'8.7'),		
+array('title'=>'Goodfellas (1990)','rating'=>'8.7'),		
+array('title'=>'The Matrix (1999)','rating'=>'8.7'),		
+array('title'=>'Seven Samurai (1954)','rating'=>'8.6'),		
+array('title'=>'Star Wars: Episode IV - A New Hope (1977)','rating'=>'8.6'),		
+array('title'=>'City of God (2002)','rating'=>'8.6'),		
+array('title'=>'Se7en (1995)','rating'=>'8.6'),		
+array('title'=>'The Silence of the Lambs (1991)','rating'=>'8.6'),		
+array('title'=>'Its a Wonderful Life (1946)','rating'=>'8.6'),		
+array('title'=>'The Usual Suspects (1995)','rating'=>'8.6'),		
+array('title'=>'Life Is Beautiful (1997)','rating'=>'8.6'),		
+array('title'=>'Leon (1994)','rating'=>'8.6'),		
+array('title'=>'Once Upon a Time in the West (1968)','rating'=>'8.5'),		
+array('title'=>'Spirited Away (2001)','rating'=>'8.5'),		
+array('title'=>'Saving Private Ryan (1998)','rating'=>'8.5'),		
+array('title'=>'American History X (1998)','rating'=>'8.5'),		
+array('title'=>'Interstellar (2014)','rating'=>'8.5'),		
+array('title'=>'Casablanca (1942)','rating'=>'8.5'),		
+array('title'=>'Psycho (1960)','rating'=>'8.5'),		
+array('title'=>'City Lights (1931)','rating'=>'8.5'),		
+array('title'=>'Raiders of the Lost Ark (1981)','rating'=>'8.5'),		
+array('title'=>'Untouchable (2011)','rating'=>'8.5'),		
+array('title'=>'Modern Times (1936)','rating'=>'8.5'),		
+array('title'=>'Rear Window (1954)','rating'=>'8.5'),		
+array('title'=>'The Green Mile (1999)','rating'=>'8.5'),		
+array('title'=>'Terminator 2 Judgment Day (1991)','rating'=>'8.5'),		
+array('title'=>'The Pianist (2002)','rating'=>'8.5'),		
+array('title'=>'The Departed (2006)','rating'=>'8.5'),		
+array('title'=>'Back to the Future (1985)','rating'=>'8.5'),		
+array('title'=>'Whiplash (2014)','rating'=>'8.5'),		
+array('title'=>'Gladiator (2000)','rating'=>'8.5'),		
+array('title'=>'Memento (2000)','rating'=>'8.5'),		
+array('title'=>'Apocalypse Now (1979)','rating'=>'8.5'),		
+array('title'=>'The Prestige (2006)','rating'=>'8.5'),		
+array('title'=>'The Lion King (1994)','rating'=>'8.4'),		
+array('title'=>'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb (1964)','rating'=>'8.4'),		
+array('title'=>'Sunset Boulevard (1950)','rating'=>'8.4'),		
+array('title'=>'Alien (1979)','rating'=>'8.4'),		
+array('title'=>'The Great Dictator (1940)','rating'=>'8.4'),		
+array('title'=>'The Lives of Others (2006)','rating'=>'8.4'),		
+array('title'=>'Cinema Paradiso (1988)','rating'=>'8.4'),		
+array('title'=>'Django Unchained (2012)','rating'=>'8.4'),		
+array('title'=>'Paths of Glory (1957)','rating'=>'8.4'),		
+array('title'=>'The Shining (1980)','rating'=>'8.4'),		
+array('title'=>'Grave of the Fireflies (1988)','rating'=>'8.4'),		
+array('title'=>'WALL·E (2008)','rating'=>'8.4'),		
+array('title'=>'The Dark Knight Rises (2012)','rating'=>'8.4'),		
+array('title'=>'American Beauty (1999)','rating'=>'8.4'),		
+array('title'=>'Aliens (1986)','rating'=>'8.4'),		
+array('title'=>'Princess Mononoke (1997)','rating'=>'8.4'),		
+array('title'=>'Oldboy (2003)','rating'=>'8.4'),		
+array('title'=>'Citizen Kane (1941)','rating'=>'8.4'),		
+array('title'=>'Once Upon a Time in America (1984)','rating'=>'8.4'),		
+array('title'=>'North by Northwest (1959)','rating'=>'8.4'),		
+array('title'=>'Das Boot (1981)','rating'=>'8.3'),		
+array('title'=>'Vertigo (1958)','rating'=>'8.3'),		
+array('title'=>'Witness for the Prosecution (1957)','rating'=>'8.3'),		
+array('title'=>'Star Wars Episode VI - Return of the Jedi (1983)','rating'=>'8.3'),		
+array('title'=>'M (1931)','rating'=>'8.3'),		
+array('title'=>'Reservoir Dogs (1992)','rating'=>'8.3'),		
+array('title'=>'Braveheart (1995)','rating'=>'8.3'),		
+array('title'=>'Amelie (2001)','rating'=>'8.3'),		
+array('title'=>'Requiem for a Dream (2000)','rating'=>'8.3'),		
+array('title'=>'A Clockwork Orange (1971)','rating'=>'8.3'),		
+array('title'=>'Taxi Driver (1976)','rating'=>'8.3'),		
+array('title'=>'Double Indemnity (1944)','rating'=>'8.3'),		
+array('title'=>'Toy Story 3 (2010)','rating'=>'8.3'),		
+array('title'=>'Lawrence of Arabia (1962)','rating'=>'8.3'),		
+array('title'=>'To Kill a Mockingbird (1962)','rating'=>'8.3'),		
+array('title'=>'Eternal Sunshine of the Spotless Mind (2004)','rating'=>'8.3'),		
+array('title'=>'Full Metal Jacket (1987)','rating'=>'8.3'),		
+array('title'=>'Amadeus (1984)','rating'=>'8.3'),		
+array('title'=>'The Sting (1973)','rating'=>'8.3'),		
+array('title'=>'2001 A Space Odyssey (1968)','rating'=>'8.3'),		
+array('title'=>'Bicycle Thieves (1948)','rating'=>'8.3'),		
+array('title'=>'Singin in the Rain (1952)','rating'=>'8.3'),		
+array('title'=>'Snatch (2000)','rating'=>'8.3'),		
+array('title'=>'Toy Story (1995)','rating'=>'8.3'),		
+array('title'=>'Monty Python and the Holy Grail (1975)','rating'=>'8.3'),		
+array('title'=>'Inglourious Basterds (2009)','rating'=>'8.3'),		
+array('title'=>'The Kid (1921)','rating'=>'8.3'),		
+array('title'=>'L.A. Confidential (1997)','rating'=>'8.3'),		
+array('title'=>'For a Few Dollars More (1965)','rating'=>'8.3'),		
+array('title'=>'The Apartment (1960)','rating'=>'8.3'),		
+array('title'=>'Rashomon (1950)','rating'=>'8.3')
+);
+
+$index = 0;
+
+if(isset($_GET['index'])){
+	$index = $_GET['index'];
+}
+
+$end = $index + 10;
+
+if($index > (count($movies) -1)){
+	echo json_encode(array());
+	exit;
+}
+
+if($end > (count($movies) -1)){
+	$end = count($movies) - 1;
+}
+
+$data = array();
+for($i=$index;$i<$end;$i++){
+	$movies[$i]['type'] = 'movie';
+	$data[] = $movies[$i];
+}
+
+echo json_encode($data);
